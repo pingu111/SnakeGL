7b8cdba387702134cac88ffd007ec002e413964d
Paul <paul.turpin@telecom-sudgrenoble.eu>
2016-10-13 14:28:19 +0200

    readfile

--- readFile.cpp
+++ readFile.cpp
@@ -1,10 +1,27 @@
 #include "readFile.h"
+#include <fstream>
+#include <sstream>
+#include <assert.h>
 
 using namespace std;
 
 void ReadFile::read(const string path)
 {
-	//TODO: lire le fichier et remplir la matrice
+	ifstream infile(path);
+	int i = 0;
+	string line;
+	while (getline(infile, line))
+	{
+		++i;
+		matrixPosition.push_back(vector<int>());
+		for (auto iter = line.begin();iter < line.end();iter++)
+		{
+			assert(*iter == '0' || *iter == '1');
+			int value = *iter - '0';
+			assert(value == 0 || value == 1);
+			matrixPosition[i].push_back(*iter);
+		}
+	}
 
 	fillVectorFromMatrix();
 }
